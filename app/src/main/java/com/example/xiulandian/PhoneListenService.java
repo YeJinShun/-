79338a09e4b284f6b8d21239ac49525943bc76a3
YeJinShun <yejinshun19981@outlook.com>
2019-11-14 08:58:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiulandian/PhoneListenService.java
@@ -0,0 +1,69 @@
+package com.example.xiulandian;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 来电监听服务
+ *
+ * @author : Joh Liu
+ * @date : 2019/8/15 16:44
+ */
+public class PhoneListenService extends Service {
+
+    /*private static final String TAG = "PhoneListenService";
+
+    // 电话管理者对象
+    private TelephonyManager mTelephonyManager;
+    // 电话状态监听者
+    private MyPhoneCallListener phoneCallListener;*/
+
+    @Override
+    public void onCreate() {
+      /*  mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        phoneCallListener = new MyPhoneCallListener();
+        phoneCallListener.setPhoneListener(number -> {
+            if (!TextUtils.isEmpty(number)) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (Settings.canDrawOverlays(getApplicationContext())) {
+                        WindowsUtils.showPopupWindow(getApplicationContext(), "移动开发部-Joh\n" + number);
+                    }
+                } else {
+                    WindowsUtils.showPopupWindow(getApplicationContext(), "移动开发部-Joh\n" + number);
+                }
+            }
+        });
+        mTelephonyManager.listen(phoneCallListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        super.onCreate();*/
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+/*    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+//    @Override
+//    public void onDestroy() {
+//        // 取消来电的电话状态监听服务
+//        if (mTelephonyManager != null && myPhoneStateListener != null) {
+//            mTelephonyManager.listen(myPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+//        }
+//        super.onDestroy();
+//    }*/
+}
